a2665220682fda79e04577e39b331d1f728027fb
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-11-14 14:36:13 +0000

    add/react native init setup & react navigation stacks

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignIn from '../screens/SignIn';
+
+const AuthStack = createStackNavigator();
+
+const AuthRoutes: React.FC = () => {
+  return (
+    <AuthStack.Navigator>
+      <AuthStack.Screen name="SignIn" component={SignIn} />
+    </AuthStack.Navigator>
+  );
+};
+
+export default AuthRoutes;
